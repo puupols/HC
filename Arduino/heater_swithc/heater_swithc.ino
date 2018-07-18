cd161af2e937e36b1319ea4bc74c1a5fa70869a3
Your Name <you@example.com>
2018-07-18 21:02:43 +0300

    Arduino emergency mode for heater switch

--- Arduino/heater_swithc/heater_swithc.ino
+++ Arduino/heater_swithc/heater_swithc.ino
@@ -9,7 +9,9 @@
 const char* ssid = "OPTIC8C8D";
 const char* password = "B1DC8C8D";
 String url = "http://192.168.1.3:8080/getHeaterStatus";
-
+int errorCount;
+String switchPossition;
+String  response;
 
 void setup() {
   Serial.begin(115200);    
@@ -24,26 +26,38 @@ void setup() {
 }
 
 void loop() {
-
+  response = "";
   if(WiFi.status() == WL_CONNECTED){
     HTTPClient http;
     http.begin(url);
-    http.GET();
-    String response = http.getString();
-    http.end();
-    Serial.println(response);
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(response);
-    String HSstatus = root["status"];
-    Serial.println(HSstatus);
-    if(HSstatus == "OFF"){
-      digitalWrite(relay, HIGH);
-      Serial.println("HIGH");
+    int httpCode = http.GET();
+    // httpCode will be negative on error
+    if(httpCode > 0){
+      response = http.getString();
+      Serial.println(response);
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(response);
+      String HSstatus = root["status"];
+      Serial.println(HSstatus);
+      if(HSstatus == "OFF"){
+        digitalWrite(relay, HIGH);
+        Serial.println("HIGH");
+        errorCount = 0;
+        } else {
+          digitalWrite(relay, LOW);
+          Serial.println("LOW");
+          errorCount = 0;
+          };      
       } else {
-        digitalWrite(relay, LOW);
-        Serial.println("LOW");
-        };
-
+        errorCount = errorCount + 1;
+        Serial.println("Error reading server");
+        } 
+        
+        if(errorCount > 3){
+          digitalWrite(relay, LOW);
+          Serial.println("Switch to emergency mode");
+          }         
+    http.end();
   } 
   delay(10000);
 }
